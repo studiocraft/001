13dd0f1d5efc92cb2a345097ba65738691a36c7a
Bryan Colosky <bcolosky@gmail.com>
2016-03-18 13:30:52 -0400

    fixed width and update scrollmagic version

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,159 +19,182 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
-        var windowHeight = $(window).height();
-        var scrollController = new ScrollMagic();
+        window.onload = function () {
+          var windowScroll = $('html');
+          var overlay = document.getElementById('loaderOverlay');
+            if (overlay && overlay.parentNode && overlay.parentNode.nodeType === 1) {
+              overlay.parentNode.removeChild(overlay);
+              overlay = null;
+              windowScroll.removeClass('locked');
+            }
+          };
+          var windowHeight = $(window).height();
+          var scrollController = new ScrollMagic.Controller();
 
-        var titleSceneHeight = $('#titleAnimation').height();
-        var titleTween = TweenMax.fromTo('#titleAnimation', 0.8, {css:{ transform: 'scale(0)', opacity: 0 }}, {css:{ transform: 'scale(1)', opacity: 1}});
-        var titleScene = new ScrollScene({triggerElement: '#titleScene', offset: titleSceneHeight/2})
-          .setClassToggle('body', 'titleScene_active')
-          .setTween(titleTween)
-          .addTo(scrollController);
+          var titleSceneHeight = $('#titleAnimation').height();
+          var titleTween = new TimelineMax();
+          titleTween.add([
+            TweenMax.fromTo('#titleAnimation', 0.8, {scale:0, opacity: 0 }, {scale: 1, opacity: 1}),
+            TweenMax.staggerFromTo('#title > text', 0.2, {opacity: 0}, {opacity: 1}, 0.2),
+            TweenMax.fromTo('#subTitle', 1.8, {opacity: 0}, {opacity: 1})
+          ]);
+          var titleScene = new ScrollMagic.Scene({triggerElement: '#titleScene', offset: 0})
+            .setClassToggle('body', 'titleScene_active')
+            .setTween(titleTween)
+            .addTo(scrollController);
 
-        var stadiumSceneHeight = $('#stadiumScene').height();
-        var stadiumTween = TweenMax.staggerFromTo('[data-name="light"]', 0.5, {css:{fill: '#ffffff'}}, {css:{fill: 'transparent'}, repeat: 3, yoyo: true}, 1);
-        var stadiumScene = new ScrollScene({triggerElement: '#stadiumScene', offset: -stadiumSceneHeight/4, duration: stadiumSceneHeight})
-          .setClassToggle('body', 'stadiumScene_active')
-          .setTween(stadiumTween)
-          .addTo(scrollController);
 
-        var scoreboardSceneHeight = $('#scoreboardScene').height();
-        var scoreboardTween = TweenMax.fromTo('#scoreboardAnimation', 1.4, {css:{transform: 'translateY(100px)', opacity: 0}}, {css:{transform: 'translateY(0)', opacity: 1}});
-        var scoreboardScene = new ScrollScene({triggerElement: '#scoreboardScene', offset: -scoreboardSceneHeight/4})
-          .setClassToggle('body', 'scoreboardScene_active')
-          .setTween(scoreboardTween)
-          .addTo(scrollController);
+          var stadiumSceneHeight = $('#stadiumScene').height();
+          var stadiumTween = TweenMax.staggerFromTo('[data-name="light"]', 0.5, {css:{fill: '#ffffff'}}, {css:{fill: 'transparent'}, repeat: 3, yoyo: true}, 1);
+          var stadiumScene = new ScrollMagic.Scene({triggerElement: '#stadiumScene', offset: -stadiumSceneHeight/4, duration: stadiumSceneHeight})
+            .setClassToggle('body', 'stadiumScene_active')
+            .setTween(stadiumTween)
+            .addTo(scrollController);
 
-        var actOneSceneHeight = $('#actOneScene').height();
-        var actOneTween = new TimelineMax();
-        actOneTween.add([
-          TweenMax.fromTo('#textOneAnimation', 1.2, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
-          TweenMax.fromTo('#voiceGuyAnimation', 0.4, { css:{transform: 'translateX(100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
-          TweenMax.staggerFromTo('[data-name="comment"]', 0.8, {css:{fill: 'transparent'}}, {css:{fill: '#ffffff'}, repeat: 0 }, 0.2)
-        ]);
-        var actOneScene = new ScrollScene({ triggerElement: '#actOneScene', offset: 0})
-          .setClassToggle('body', 'actOneScene_active')
-          .setTween(actOneTween)
-          .addTo(scrollController);
+          var scoreboardSceneHeight = $('#scoreboardScene').height();
+          var scoreboardTween = TweenMax.fromTo('#scoreboardAnimation', 1.4, {css:{transform: 'translateY(100px)', opacity: 0}}, {css:{transform: 'translateY(0)', opacity: 1}});
+          var scoreboardScene = new ScrollMagic.Scene({triggerElement: '#scoreboardScene', offset: -scoreboardSceneHeight/4})
+            .setClassToggle('body', 'scoreboardScene_active')
+            .setTween(scoreboardTween)
+            .addTo(scrollController);
 
-        var actOnePinTween = TweenMax.fromTo('[data-name="playerOne"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
+          var actOneSceneHeight = $('#actOneScene').height();
+          var actOneTween = new TimelineMax();
+          actOneTween.add([
+            TweenMax.fromTo('#textOneAnimation', 1.2, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
+            TweenMax.fromTo('#voiceGuyAnimation', 0.4, { css:{transform: 'translateX(100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
+            TweenMax.staggerFromTo('[data-name="comment"]', 0.8, {css:{fill: 'transparent'}}, {css:{fill: '#ffffff'}, repeat: 0 }, 0.2)
+          ]);
+          var actOneScene = new ScrollMagic.Scene({ triggerElement: '#actOneScene', offset: 0})
+            .setClassToggle('body', 'actOneScene_active')
+            .setTween(actOneTween)
+            .addTo(scrollController);
 
-        var actOnePinHeight = $('#helmetOneScene').height();
-        var actOnePin = new ScrollScene({ triggerElement: '#helmetOneScene', offset: actOnePinHeight, duration: windowHeight})
-          .setClassToggle('body', 'helmetOneScenePin_active')
-          .setPin('#playersGroupOne')
-          .setTween(actOnePinTween)
-          .addTo(scrollController);
+          var actOnePinTween = TweenMax.fromTo('[data-name="playerOne"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
 
-        var actOneFoulTween = TweenMax.staggerFromTo('[data-foul-one="true"]', 0.6, {scale: 1, rotation: 0},{scale: 0, rotation: 360}, 1);
-        var actOneFoulScene = new ScrollScene({ triggerElement: '#helmetOneScene', offset: actOnePinHeight*1.2})
-          .setClassToggle('body', 'actOneFoul_active')
-          .setTween(actOneFoulTween)
-          .addTo(scrollController);
+          var actOnePinHeight = $('#helmetOneScene').height();
+          var actOnePin = new ScrollMagic.Scene({ triggerElement: '#helmetOneScene', offset: actOnePinHeight, duration: windowHeight})
+            .setClassToggle('body', 'helmetOneScenePin_active')
+            .setPin('#playersGroupOne')
+            .setTween(actOnePinTween)
+            .addTo(scrollController);
 
-        var actTwoSceneHeight = $('#actTwoScene').height();
-        var actTwoTween = new TimelineMax();
-        actTwoTween.add([
-          TweenMax.fromTo('#textTwoAnimation', 1.8, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
-          TweenMax.staggerFromTo('[data-name="lightbulb"]', 0.4, {css:{opacity: '0'}}, {css:{opacity: '1'}, repeat: 0 }, 0.2),
-          TweenMax.fromTo('#brainGuyAnimation', 0.8, { css:{transform: 'translateX(100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}})
-        ]);
-        var actTwoScene = new ScrollScene({ triggerElement: '#actTwoScene', offset: -actTwoSceneHeight/3})
-          .setClassToggle('body', 'actTwoScene_active')
-          .setTween(actTwoTween)
-          .addTo(scrollController);
+          var actOneFoulTween = TweenMax.staggerFromTo('[data-foul-one="true"]', 0.6, {scale: 1, rotation: 0},{scale: 0, rotation: 360}, 1);
+          var actOneFoulScene = new ScrollMagic.Scene({ triggerElement: '#helmetOneScene', offset: actOnePinHeight*1.2})
+            .setClassToggle('body', 'actOneFoul_active')
+            .setTween(actOneFoulTween)
+            .addTo(scrollController);
 
-        var actTwoPinTween = TweenMax.fromTo('[data-name="playerTwo"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
+          var actTwoSceneHeight = $('#actTwoScene').height();
+          var actTwoTween = new TimelineMax();
+          actTwoTween.add([
+            TweenMax.fromTo('#textTwoAnimation', 1.8, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
+            TweenMax.staggerFromTo('[data-name="lightbulb"]', 0.4, {css:{opacity: '0'}}, {css:{opacity: '1'}, repeat: 0 }, 0.2),
+            TweenMax.fromTo('#brainGuyAnimation', 0.8, { css:{transform: 'translateX(100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}})
+          ]);
+          var actTwoScene = new ScrollMagic.Scene({ triggerElement: '#actTwoScene', offset: -actTwoSceneHeight/3})
+            .setClassToggle('body', 'actTwoScene_active')
+            .setTween(actTwoTween)
+            .addTo(scrollController);
 
-        var actTwoPinHeight = $('#helmetTwoScene').height();
-        var actTwoPin = new ScrollScene({ triggerElement: '#helmetTwoScene', offset: actTwoPinHeight, duration: windowHeight})
-          .setClassToggle('body', 'helmetTwoScenePin_active')
-          .setPin('#playersGroupTwo')
-          .setTween(actTwoPinTween)
-          .addTo(scrollController);
+          var actTwoPinTween = TweenMax.fromTo('[data-name="playerTwo"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
 
-        var actTwoFoulTween = TweenMax.staggerFromTo('[data-foul-two="true"]', 0.6, {scale: 1, rotation: 0},{scale: 0, rotation: 360}, 1);
-        var actTwoFoulScene = new ScrollScene({ triggerElement: '#helmetTwoScene', offset: actTwoPinHeight*1.2})
-          .setClassToggle('body', 'actTwoFoul_active')
-          .setTween(actTwoFoulTween)
-          .addTo(scrollController);
+          var actTwoPinHeight = $('#helmetTwoScene').height();
+          var actTwoPin = new ScrollMagic.Scene({ triggerElement: '#helmetTwoScene', offset: actTwoPinHeight, duration: windowHeight})
+            .setClassToggle('body', 'helmetTwoScenePin_active')
+            .setPin('#playersGroupTwo')
+            .setTween(actTwoPinTween)
+            .addTo(scrollController);
 
-        var actThreeSceneHeight = $('#actThreeScene').height();
-        var actThreeTween = new TimelineMax();
-        actThreeTween.add([
-          TweenMax.fromTo('#textThreeAnimation', 1.4, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
-          TweenMax.fromTo('[data-name="heart"]', 0.8, {css:{strokeWidth: '1rem', stroke: '#FF4338'}}, {css:{strokeWidth: '0', stroke: '#FF4338'}, repeat: -1, yoyo: true }, 1),
-          TweenMax.fromTo('#heartGuyAnimation', 0.8, { css:{transform: 'translateX(100px)', opacity: 0}}, {css:{transform: 'translateX(0px)', opacity: 1}})
-        ]);
-        var actThreeScene = new ScrollScene({ triggerElement: '#actThreeScene', offset: 0})
-          .setClassToggle('body', 'actThreeScene_active')
-          .setTween(actThreeTween)
-          .addTo(scrollController);
+          var actTwoFoulTween = new TimelineMax();
+          actTwoFoulTween.add([
+            TweenMax.staggerFromTo('[data-foul-two="true"]', 0.6, {scale: 1, rotation: 0},{scale: 0, rotation: 360}, 1),
+            TweenMax.set('[data-foul-two="true"]', {display: 'none'}),
+            TweenMax.set('[data-name="flag"]', {display: 'block'}),
+            TweenMax.staggerFromTo('[data-name="flag"]', 0.6, {scale: 1, rotation: 0},{scale: 0, rotation: 360}, 1),
+          ]);
 
-        var actThreePinTween = TweenMax.fromTo('[data-name="playerThree"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
+          var actTwoFoulScene = new ScrollMagic.Scene({ triggerElement: '#helmetTwoScene', offset: actTwoPinHeight*1.2})
+            .setClassToggle('body', 'actTwoFoul_active')
+            .setTween(actTwoFoulTween)
+            .addTo(scrollController);
 
-        var actThreePinHeight = $('#helmetThreeScene').height();
-        var actThreePin = new ScrollScene({ triggerElement: '#helmetThreeScene', offset: actThreePinHeight, duration: windowHeight})
-          .setClassToggle('body', 'helmetThreeScenePin_active')
-          .setPin('#playersGroupThree')
-          .setTween(actThreePinTween)
-          .addTo(scrollController);
+          var actThreeSceneHeight = $('#actThreeScene').height();
+          var actThreeTween = new TimelineMax();
+          actThreeTween.add([
+            TweenMax.fromTo('#textThreeAnimation', 1.4, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
+            TweenMax.fromTo('[data-name="heart"]', 0.8, {css:{strokeWidth: '1rem', stroke: '#FF4338'}}, {css:{strokeWidth: '0', stroke: '#FF4338'}, repeat: -1, yoyo: true }, 1),
+            TweenMax.fromTo('#heartGuyAnimation', 0.8, { css:{transform: 'translateX(100px)', opacity: 0}}, {css:{transform: 'translateX(0px)', opacity: 1}})
+          ]);
+          var actThreeScene = new ScrollMagic.Scene({ triggerElement: '#actThreeScene', offset: 0})
+            .setClassToggle('body', 'actThreeScene_active')
+            .setTween(actThreeTween)
+            .addTo(scrollController);
 
-        var actThreeFoulTween = TweenMax.staggerFromTo('[data-foul-three="true"]', 0.6, {scale: 1, rotation: 0},{scale: 0, rotation: 360}, 1);
-        var actThreeFoulScene = new ScrollScene({ triggerElement: '#helmetThreeScene', offset: actThreePinHeight*1.2})
-          .setClassToggle('body', 'actThreeFoul_active')
-          .setTween(actThreeFoulTween)
-          .addTo(scrollController);
+          var actThreePinTween = TweenMax.fromTo('[data-name="playerThree"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
 
-        var encoreSceneHeight = $('#encoreScene').height();
-        var encoreTween = new TimelineMax();
-        encoreTween.add([
-          TweenMax.fromTo('#encoreTextAnimation', 1.4, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
-          TweenMax.fromTo('#goalPostAnimation', 0.8, {css:{transform: 'translateY(200px)', opacity: 0}}, {css:{transform: 'translateY(0)', opacity: 1}})
-        ]);
-        var encoreScene = new ScrollScene({ triggerElement: '#encoreScene', offset: 0})
-          .setClassToggle('body', 'encoreScene_active')
-          .setTween(encoreTween)
-          .addTo(scrollController);
+          var actThreePinHeight = $('#helmetThreeScene').height();
+          var actThreePin = new ScrollMagic.Scene({ triggerElement: '#helmetThreeScene', offset: actThreePinHeight, duration: windowHeight})
+            .setClassToggle('body', 'helmetThreeScenePin_active')
+            .setPin('#playersGroupThree')
+            .setTween(actThreePinTween)
+            .addTo(scrollController);
 
-        var encorePinTween = TweenMax.fromTo('[data-name="playerFour"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
+          var actThreeFoulTween = TweenMax.staggerFromTo('[data-foul-three="true"]', 0.6, {scale: 1, rotation: 0},{scale: 0, rotation: 360}, 0.2);
+          var actThreeFoulScene = new ScrollMagic.Scene({ triggerElement: '#helmetThreeScene', offset: actThreePinHeight*1.2})
+            .setClassToggle('body', 'actThreeFoul_active')
+            .setTween(actThreeFoulTween)
+            .addTo(scrollController);
 
-        var encorePinHeight = $('#helmetFourScene').height();
-        var encorePin = new ScrollScene({ triggerElement: '#helmetFourScene', offset: actThreePinHeight, duration: encorePinHeight})
-          .setClassToggle('body', 'helmetFourScenePin_active')
-          .setPin('#playersGroupFour')
-          .setTween(encorePinTween)
-          .addTo(scrollController);
+          var encoreSceneHeight = $('#encoreScene').height();
+          var encoreTween = new TimelineMax();
+          encoreTween.add([
+            TweenMax.fromTo('#encoreTextAnimation', 1.4, {css:{transform: 'translateX(-100px)', opacity: 0}}, {css:{transform: 'translateX(0)', opacity: 1}}),
+            TweenMax.fromTo('#goalPostAnimation', 0.8, {css:{transform: 'translateY(200px)', opacity: 0}}, {css:{transform: 'translateY(0)', opacity: 1}})
+          ]);
+          var encoreScene = new ScrollMagic.Scene({ triggerElement: '#encoreScene', offset: 0})
+            .setClassToggle('body', 'encoreScene_active')
+            .setTween(encoreTween)
+            .addTo(scrollController);
 
-        var chalkBoardHeight = $('#chalkBoardScene').height();
-        var chalkBoardTween = TweenMax.fromTo('#chalkBoardAnimation', 1.4, {css:{transform: 'translateY(-240px)', opacity: 0}}, {css:{transform: 'translateY(0)', opacity: 1}});
-        var recapScene = new ScrollScene({triggerElement: '#chalkBoardScene', offset: -chalkBoardHeight/3})
-          .setClassToggle('body', 'chalkBoardScene_active')
-          .setTween(chalkBoardTween)
-          .addTo(scrollController);
+          var encorePinTween = TweenMax.fromTo('[data-name="playerFour"]', 0.2, {css:{transform: 'rotate(-5deg)'}}, {css:{transform: 'rotate(5deg)'},repeat: -1, yoyo: true}, 2);
 
-        // Add debug indicators fixed on right side
-        titleScene.addIndicators();
-        stadiumScene.addIndicators();
-        scoreboardScene.addIndicators();
+          var encorePinHeight = $('#helmetFourScene').height();
+          var encorePin = new ScrollMagic.Scene({ triggerElement: '#helmetFourScene', offset: encorePinHeight, duration: encorePinHeight*2})
+            .setClassToggle('body', 'helmetFourScenePin_active')
+            .setPin('#playersGroupFour')
+            .setTween(encorePinTween)
+            .addTo(scrollController);
 
-        actOneScene.addIndicators();
-        actOnePin.addIndicators();
-        actOneFoulScene.addIndicators();
+          var chalkBoardHeight = $('#chalkBoardScene').height();
+          var chalkBoardTween = TweenMax.fromTo('#chalkBoardAnimation', 1.4, {css:{transform: 'translateY(-240px)', opacity: 0}}, {css:{transform: 'translateY(0)', opacity: 1}});
+          var recapScene = new ScrollMagic.Scene({triggerElement: '#chalkBoardScene', offset: -chalkBoardHeight/3})
+            .setClassToggle('body', 'chalkBoardScene_active')
+            .setTween(chalkBoardTween)
+            .addTo(scrollController);
 
-        actTwoScene.addIndicators();
-        actTwoPin.addIndicators();
-        actTwoFoulScene.addIndicators();
+          // Add debug indicators fixed on right side
+          titleScene.addIndicators();
+          stadiumScene.addIndicators();
+          scoreboardScene.addIndicators();
 
-        actThreeScene.addIndicators();
-        actThreePin.addIndicators();
-        actThreeFoulScene.addIndicators();
+          actOneScene.addIndicators();
+          actOnePin.addIndicators();
+          actOneFoulScene.addIndicators();
 
-        encoreScene.addIndicators();
-        encorePin.addIndicators();
+          actTwoScene.addIndicators();
+          actTwoPin.addIndicators();
+          actTwoFoulScene.addIndicators();
 
-        recapScene.addIndicators();
+          actThreeScene.addIndicators();
+          actThreePin.addIndicators();
+          actThreeFoulScene.addIndicators();
+
+          encoreScene.addIndicators();
+          encorePin.addIndicators();
+
+          // goalPostScene.addIndicators();
+          recapScene.addIndicators();
 
       },
       finalize: function() {
